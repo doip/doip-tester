055c3c46e2942ffae6716b8cb2865b68cf8915a1
Marco Wehnert <marco.wehnert@gmx.de>
2022-11-09 20:30:16 +0100

    Changed some logger packages, added stuff for sending email, added new Gradle task 'testFast', that will exclude tests which are tagged with 'slow'.

---
+++ src/test/java/doip/tester/testrunner/TR_9030_InitialInactivityTimer.java
@@ -0,0 +1,7 @@
+package doip.tester.testrunner;
+
+import doip.tester.testcases.TC_9030_InitialInactivityTimer;
+
+public class TR_9030_InitialInactivityTimer extends TC_9030_InitialInactivityTimer {
+
+}
