48c8ea85d88821574e3e367d26dd26826365c770
Marco Wehnert <marco.wehnert@gmx.de>
2022-12-09 13:44:49 +0100

    Added test runner for TC-2110

---
+++ src/test/java/doip/tester/testrunner/TR_2110_UnknownTargetAddress.java
@@ -0,0 +1,7 @@
+package doip.tester.testrunner;
+
+import doip.tester.testcases.TC_2110_UnknownTargetAddress;
+
+public class TR_2110_UnknownTargetAddress extends TC_2110_UnknownTargetAddress {
+
+}
